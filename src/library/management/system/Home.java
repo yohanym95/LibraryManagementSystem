981ef622412fb41b9182454e0e729bb580c17c57
yohanym95 <malshikay@gmail.com>
2018-07-12 20:12:32 +0530

    Added new student page

--- src/library/management/system/Home.java
+++ src/library/management/system/Home.java
@@ -146,7 +146,9 @@ public class Home extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+       NewStudent n1 = new NewStudent();
+       n1.setVisible(true);
+       this.dispose();
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
